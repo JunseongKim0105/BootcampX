af9102c273acb1f9b922a5c2f0b510199f54391a
JunseongKim0105 <juns.kim0105@gmail.com>
2023-02-09 01:52:46 -0500

    app

---
+++ bootcamp_app/students.js
@@ -0,0 +1,29 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx',
+});
+
+const [cohortName, limit] = process.argv.slice(2);
+const values = [`%${cohortName}%`, `${limit || 5}`];
+
+pool
+  .query(
+    `
+SELECT id, name, cohort_id
+FROM students
+JOIN cohorts ON cohorts_id = cohort_id
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`
+  )
+  .then((res) => {
+    res.rows.forEach((user) => {
+      console.log(
+        `${user.name} has an id of ${user.id} and was in the ${user.cohort_id} cohort`
+      );
+    });
+  });
---
+++ bootcamp_app/teachers.js
@@ -0,0 +1,29 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: 'pw',
+  host: 'localhost',
+  database: 'bootcampx',
+});
+
+const values = [`${process.argv[2] || 'JUL02'}`];
+
+const queryString = `
+SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+FROM teachers
+JOIN assistance_requests ON teacher_id = teachers.id
+JOIN students ON student_id = students.id
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.name = $1
+ORDER BY teacher;
+`;
+
+pool
+  .query(queryString, values)
+  .then((res) =>
+    res.rows.forEach((teacher) =>
+      console.log(`${teacher.cohort}: ${teacher.teacher}`)
+    )
+  )
+  .catch((err) => console.error('query error', err.stack));
